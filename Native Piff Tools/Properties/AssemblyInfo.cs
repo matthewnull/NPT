189e6060f55f084feccbb8aeb60319c4aa29bc9b
piffgram <support@piffgram.tk>
2023-03-05 21:32:45 -0800

    Add files via upload
    Signed-off-by: piffgram <support@piffgram.tk>

---
+++ Native Piff Tools/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Native Piff Tools")]
+[assembly: AssemblyDescription("Multi-Tool For PC")]
+[assembly: AssemblyConfiguration("Retail")]
+[assembly: AssemblyCompany("piffgram")]
+[assembly: AssemblyProduct("Native Piff Tools")]
+[assembly: AssemblyCopyright("Copyright © piffgram 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("32201d7f-dcc5-4e75-9a38-423b8e363794")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
