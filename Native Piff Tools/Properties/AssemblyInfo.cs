4325b5e7fac38b965b15802989a30cedc70a5ae6
piffgram <support@piffgram.tk>
2023-03-06 22:45:48 -0800

    Update AssemblyInfo.cs

--- Native Piff Tools/Properties/AssemblyInfo.cs
+++ Native Piff Tools/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Native Piff Tools")]
-[assembly: AssemblyDescription("Multi-Tool For PC")]
-[assembly: AssemblyConfiguration("Retail")]
-[assembly: AssemblyCompany("piffgram")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Piff")]
 [assembly: AssemblyProduct("Native Piff Tools")]
-[assembly: AssemblyCopyright("Copyright © piffgram 2023")]
+[assembly: AssemblyCopyright("Copyright © Piff 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
